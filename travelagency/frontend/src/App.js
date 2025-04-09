c7393f50faa7612f3846555081b0749adeb1f218
elsaferati <elsavferati@gmail.com>
2025-04-09 21:21:35 +0200

    update app.js

--- travelagency/frontend/src/App.js
+++ travelagency/frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
+
 
