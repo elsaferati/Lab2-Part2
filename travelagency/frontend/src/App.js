83dda5a8fc0614a98ac6efd4d16a2d9d0640a4ad
Vjona <lekajvjona@hotmail.com>
2025-04-10 23:08:45 +0200

    Update App.js

--- travelagency/frontend/src/App.js
+++ travelagency/frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
+import HelpCenter from './pages/HelpCenter';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-      
-      {/* {showSignUp && (
-        <SignUp onClose={() => setShowSignUp(false)} />
-      )} */}
-    </div>
-
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/help-center" element={<HelpCenter />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
