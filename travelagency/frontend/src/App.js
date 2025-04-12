fc9ac84ad9179b2dc3fe482491c2baeef96f0323
Vjona <lekajvjona@hotmail.com>
2025-04-12 02:01:33 +0200

    changes

--- travelagency/frontend/src/App.js
+++ travelagency/frontend/src/App.js
@@ -7,7 +7,7 @@ import SignUp from './components/SignUp';
 import Register from './components/Register';
 import HelpCenter from './pages/HelpCenter';
 import CheckInBooking from './pages/CheckInBooking';
-import Services from './pages/Services'; 
+import Services from './pages/Services';
 import AirportParking from './pages/AirportParking';
 import FastTrack from './pages/FastTrack';
 import AirportLounge from './pages/AirportLounge';
@@ -15,25 +15,16 @@ import TravelInsurance from './pages/TravelInsurance';
 import Priorities from './pages/Priorities';
 import FareLock from './pages/FareLock';
 import TravelInsurancePage from './pages/Bundles';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import Discounts  from './pages/Discounts';
-import Partner  from './pages/Partner';
-=======
-=======
->>>>>>> Stashed changes
+import Discounts from './pages/Discounts';
+import Partner from './pages/Partner';
 import ChangeBooking from './pages/faq/ChangeBooking';
 import BaggagePolicy from './pages/faq/BaggagePolicy';
 import RequestRefund from './pages/faq/RequestRefund';
 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
 function App() {
   const [showSignUp, setShowSignUp] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
+
   return (
     <Router>
       <div className="min-h-screen flex flex-col relative">
@@ -43,46 +34,36 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/help-center" element={<HelpCenter />} />
-            <Route path="/check-in-booking" element={<CheckInBooking />} /> 
-            <Route path="/services" element={<Services />} /> 
-            <Route path="/airport-parking" element={<AirportParking />} /> 
+            <Route path="/check-in-booking" element={<CheckInBooking />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/airport-parking" element={<AirportParking />} />
             <Route path="/fast-track" element={<FastTrack />} />
             <Route path="/lounge" element={<AirportLounge />} />
             <Route path="/travel-insurance" element={<TravelInsurance />} />
             <Route path="/priorities" element={<Priorities />} />
             <Route path="/services/fare-lock" element={<FareLock />} />
             <Route path="/bundles" element={<TravelInsurancePage />} />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             <Route path="/services/discounts" element={<Discounts />} />
             <Route path="/services/partners" element={<Partner />} />
-=======
-=======
->>>>>>> Stashed changes
             <Route path="/faq/change-booking" element={<ChangeBooking />} />
             <Route path="/faq/baggage-policy" element={<BaggagePolicy />} />
             <Route path="/faq/request-refund" element={<RequestRefund />} />
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
           </Routes>
         </main>
         <Footer />
         {showSignUp && (
-        <SignUp
-          onClose={() => setShowSignUp(false)}
-          onRegisterClick={() => {
-            setShowSignUp(false);
-            setShowRegister(true);
-          }}
-        />
-      )}
+          <SignUp
+            onClose={() => setShowSignUp(false)}
+            onRegisterClick={() => {
+              setShowSignUp(false);
+              setShowRegister(true);
+            }}
+          />
+        )}
 
-      {showRegister && (
-        <Register onClose={() => setShowRegister(false)} />
-      )}
+        {showRegister && (
+          <Register onClose={() => setShowRegister(false)} />
+        )}
       </div>
     </Router>
   );
